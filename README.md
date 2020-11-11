f9d2a36b3e63e9f9f020b68d46870d25d29e25f8
gwelle <guillaume.welle.sio@gmail.com>
2020-11-11 16:08:05 +0100

    Correction du prof Exo2 vérif type/valeur

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # esgi-javascript
 docker-compose run node node ./intro/hello.js
+
 docker-compose run node node ./exercise-1/string.js
+
 docker-compose run node node ./exercise-2/type-check.js
--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -47,6 +47,79 @@ const personne = new Personne('WELLE');
 console.log(type_check_v1(personne,'object'));//true
 console.log(type_check_v1(personne,'function'));//false
 
-//En JS, les tableaux sont des objets => ['v','n'] est un objet de type Array
+//En JS, les tableaux, null, function sont des objets => ['v','n'] est un objet de type Array
 
 
+//CORRECTION DU PROF => EXERCICE N°1
+function type_check_v1_bis(variable, type) {
+  const typeOfVariable = typeof variable;
+
+  switch (typeOfVariable) {
+    case "object":
+      switch (type) {
+        case "null":
+          return variable === null;
+        case "array":
+          return Array.isArray(variable);
+        case "object":
+          return variable !== null && !Array.isArray(variable);
+        default:
+          return false;
+      }
+    default:
+      return typeOfVariable === type;
+  }
+}
+
+console.log('--------------------------------------');
+console.log(type_check_v1_bis({}, "null"));
+console.log(type_check_v1_bis({}, "object"));
+console.log(type_check_v1_bis({}, "array"));
+console.log(type_check_v1_bis(null, "object"));
+console.log(type_check_v1_bis(null, "null"));
+console.log(type_check_v1_bis({}, "number"));
+console.log(type_check_v1_bis([], "array"));
+console.log(type_check_v1_bis([], "object"));
+
+//CORRECTION DU PROF => EXERCICE N°2
+//type_check_v2(variable,conf => object )
+//par de l'ojbet 
+  //switch sur les clés 
+  
+function type_check_v2(variable, conf) {
+  
+
+
+  for (key in conf) {
+
+
+    switch (key) {
+      case "type":
+        if (!type_check_v1_bis(variable, conf.type)) return false;//Vérification du type =>  return false 
+        break;
+      case "value":
+      	//JSON.stringify => convertir un objet en string
+        //value => une égalite entre 2 JSON.stringify
+        //Si la valeur courante != valeur dans l'objet => return false
+        if (JSON.stringify(variable) !== JSON.stringify(conf.value)) return false;
+        break;
+      //Enum, value supplémentaire
+      case "enum":
+        enum_loop: {
+          for (subValue of conf.enum) {
+          
+            if (type_check_v2(variable, { value: subValue })) {
+              break enum_loop;
+            }
+          }
+          return false;
+        }
+    }
+  }
+
+  return true;
+}
+console.log('--------------------------------------');
+console.log(
+  type_check_v2({ e: 4 }, { type: "object", enum: [3, "test", { e: 4 }] })
+);
